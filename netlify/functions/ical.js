96af200dbe587edcb15e441f6302455f7832a0e9
kimcombs253-lab <kimcombs253@gmail.com>
2026-06-03 20:58:41 -0700

    Create ical.js

---
+++ netlify/functions/ical.js
@@ -0,0 +1,26 @@
+const https = require('https');
+const http = require('http');
+
+exports.handler = async function(event) {
+  const url = event.queryStringParameters && event.queryStringParameters.url;
+  if (!url) return { statusCode: 400, body: 'Missing url parameter' };
+
+  return new Promise((resolve) => {
+    const client = url.startsWith('https') ? https : http;
+    client.get(url, (res) => {
+      let data = '';
+      res.on('data', chunk => data += chunk);
+      res.on('end', () => resolve({
+        statusCode: 200,
+        headers: {
+          'Content-Type': 'text/calendar',
+          'Access-Control-Allow-Origin': '*'
+        },
+        body: data
+      }));
+    }).on('error', (e) => resolve({
+      statusCode: 500,
+      body: 'Fetch error: ' + e.message
+    }));
+  });
+};
